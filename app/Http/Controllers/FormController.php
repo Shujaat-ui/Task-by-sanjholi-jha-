a205281bacc932eee18c8a470e539181bd978ec0
SHUJAAT ALI <shujaatali250@gmail.com>
2025-12-12 00:18:55 +0530

    Update FormController.php

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -7,5 +7,28 @@ use Illuminate\Http\Request;
 
 class FormController extends Controller
 {
-    //
+
+    public function showForm() {
+        return view('custom_form');
+    }
+
+     public function submitForm(Request $request) {
+       $validated = $request->validate([
+        full_name => 'required|string|max:255',
+        email     =>'required|string',
+        mobile     =>'required|string|max:10',
+        address     =>'required|string',
+        dob     =>'required|string',
+        course     =>'required|string',
+        university     =>'required|string',
+        passport     =>'required|string',
+       ]);
+
+       custom_form_data::create($validated);
+
+       Mail::to('admin@gmail.com')->send(new AdminNotice($validated));
+        Mail::to($validated(email))->send(new userNotice ($validated));
+
+        return back()-with('success' 'Form is submitted succesfully')
+    }
 }
